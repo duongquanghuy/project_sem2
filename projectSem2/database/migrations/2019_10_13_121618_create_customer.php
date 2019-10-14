2adb1955bda3d1736fd4f9c1a77193e50dab00c2
duong qaung huy <huydqth1810012@fpt.edu.vn>
2019-10-14 18:48:14 +0700

    up_project

---
+++ projectSem2/database/migrations/2019_10_13_121618_create_customer.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->bigIncrements('customer_id');
+            $table->string('customer_fullname')->length(250);
+            $table->double('customer_phone')->length(15);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
