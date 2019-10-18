6e4902e0826735bfffd5f25d77edb87e09b0eb2e
duong qaung huy <huydqth1810012@fpt.edu.vn>
2019-10-18 14:23:52 +0700

    upload-database-18-10

---
+++ database/migrations/2019_10_13_100133_create_product.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->string('product_id')->primary()->length(30);
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')
+                    ->references('category_id')
+                    ->on('product_category');
+            $table->string('product_name')->length(400);
+            $table->string('link_img');
+            $table->boolean('exp_status');
+            $table->integer('price');
+            $table->double('quantity');
+            $table->double('product_on_store')->length(30);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
